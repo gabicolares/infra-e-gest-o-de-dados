4be94b66f208ac6f108ff3d73900ba5e6c2f6661
gabicolares <gabriele.severino@edu.pucrs.br>
2024-04-22 03:26:40 -0300

    .

--- t1/clusters.sql
+++ t1/clusters.sql
@@ -1,34 +1,40 @@
 -- passengers-passengers_details
-CREATE CLUSTER cl_passengers(passenger_id numeric(12)) INDEX;
+CREATE CLUSTER clr_passengers(passenger_id numeric(12)) INDEX
+    storage (
+        INITIAL 8K
+    );
     
-CREATE INDEX idx_cl_passengers ON CLUSTER cl_passengers;
+CREATE INDEX idx_clr_passengers ON CLUSTER clr_passengers;
 
 DROP TABLE AIR_PASSENGERS_DETAILS CASCADE CONSTRAINTS;
 DROP TABLE AIR_PASSENGERS CASCADE CONSTRAINTS;
 
-CREATE TABLE AIR_PASSENGERS CLUSTER cl_passengers(passenger_id) AS SELECT * FROM arruda.AIR_PASSENGERS;
-CREATE TABLE AIR_PASSENGERS_DETAILS CLUSTER cl_passengers(passenger_id) AS SELECT * FROM arruda.AIR_PASSENGERS_DETAILS;
+CREATE TABLE AIR_PASSENGERS CLUSTER clr_passengers(passenger_id) AS SELECT * FROM arruda.AIR_PASSENGERS;
+CREATE TABLE AIR_PASSENGERS_DETAILS CLUSTER clr_passengers(passenger_id) AS SELECT * FROM arruda.AIR_PASSENGERS_DETAILS;
 
 
 -- flights-bookings
-CREATE CLUSTER cl_flights_bookings(flight_id numeric(10)) INDEX;
+CREATE CLUSTER clr_flights_bookings(flight_id numeric(10)) INDEX
+    storage (
+        INITIAL 8K
+    );
 
-CREATE INDEX idx_cl_flights ON CLUSTER cl_flights_bookings;
+CREATE INDEX idx_clr_flights ON CLUSTER clr_flights_bookings;
 
 DROP TABLE AIR_BOOKINGS CASCADE CONSTRAINTS;
 DROP TABLE AIR_FLIGHTS CASCADE CONSTRAINTS;
 
-CREATE TABLE AIR_FLIGHTS CLUSTER cl_flights_bookings(flight_id) AS SELECT * FROM arruda.AIR_FLIGHTS;
-CREATE TABLE AIR_BOOKINGS CLUSTER cl_flights_bookings(flight_id) AS SELECT * FROM arruda.AIR_BOOKINGS;
+CREATE TABLE AIR_FLIGHTS CLUSTER clr_flights_bookings(flight_id) AS SELECT * FROM arruda.AIR_FLIGHTS;
+CREATE TABLE AIR_BOOKINGS CLUSTER clr_flights_bookings(flight_id) AS SELECT * FROM arruda.AIR_BOOKINGS;
  
 -- airports-passenger_details
-CREATE CLUSTER cl_airports(airport_id numeric(5)) HASHKEYS 128
+CREATE CLUSTER clr_airports(airport_id numeric(5)) HASHKEYS 128
     storage (
         INITIAL 1024K
     );
     
-DROP TABLE AIR_PASSENGERS_DETAILS CASCADE CONSTRAINTS;
+DROP TABLE AIR_AIRPORTS_GEO CASCADE CONSTRAINTS;
 DROP TABLE AIR_AIRPORTS CASCADE CONSTRAINTS;
 
-CREATE TABLE AIR_AIRPORTS CLUSTER cl_airports(airport_id) AS SELECT * FROM arruda.AIR_AIRPORTS;
-CREATE TABLE AIR_PASSENGERS_DETAILS CLUSTER cl_airports(airport_id) AS SELECT * FROM arruda.AIR_PASSENGERS_DETAILS;
+CREATE TABLE AIR_AIRPORTS CLUSTER clr_airports(airport_id) AS SELECT * FROM arruda.AIR_AIRPORTS;
+CREATE TABLE AIR_AIRPORTS_GEO CLUSTER clr_airports(airport_id) AS SELECT * FROM arruda.AIR_AIRPORTS_GEO;
