ebf26ed0f2e4219f60cdf084c58a37d0b26051e9
gabicolares <gabriele.severino@edu.pucrs.br>
2024-06-12 16:08:31 -0300

    12/06

--- t2/getInserts.sql
+++ t2/getInserts.sql
@@ -21,20 +21,6 @@ FROM
     FETCH FIRST 20000 ROWS ONLY
 ;
 
-SELECT DISTINCT
-    'insert into q1q2.Bookings(flight_id,booking_id,price,seat,passenger_id) values(' ||
-    TRIM(f.flight_id) || ',' ||
-    b.booking_id || ',' ||
-    TRIM(TO_CHAR(b.price, '9999999999D99', 'NLS_NUMERIC_CHARACTERS = ''.,''')) || ',' ||
-    '''' || TRIM(b.seat) || ''',' ||
-    p.passenger_id || ');'
-FROM
-    air_flights f
-    inner join air_bookings b ON b.flight_id = f.flight_id
-    inner join air_passengers p ON p.passenger_id = b.passenger_id
-;
-
-
 --------------------------------------------------------------------------------------------------
 
 SELECT DISTINCT
@@ -53,5 +39,5 @@ SELECT DISTINCT
 
 FROM
     air_airplanes p
-    inner join air_airplanes_types pt on p.airplane_type_id = pt.airplane_type_id
+    inner join air_airplane_types pt on p.airplane_type_id = pt.airplane_type_id
 ;
